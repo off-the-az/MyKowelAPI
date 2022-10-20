03727c64af6890739f409ce7b21c41b7e9fae91a
off-the-az <0ff.th3.az@gmail.com>
2022-10-21 00:01:00 +0300

    v. 0.1.12-1

--- src/main/java/com/az/mykowel/MyKowelApplication.java
+++ src/main/java/com/az/mykowel/MyKowelApplication.java
@@ -4,10 +4,6 @@ import com.az.mykowel.model.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.*;
-import java.util.Objects;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
